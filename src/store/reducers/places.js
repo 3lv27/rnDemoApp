cbc12ff65f1a6f89f0f4b74afb1b48752791df4d
3lv27 <elvin.gm27@gmail.com>
2019-01-15 23:28:53 +0100

    adding redux 2

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -20,6 +20,29 @@ const reducer = (state = initialState, action) => {
         })
       }
       break;
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: prevState.places.filter(place => {
+          return place.key !== state.selectedPlace.key
+        }),
+        selectedPlace: null
+      }
+      break;
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key === key
+        })
+      }
+      break;
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null
+      }
+      break
   
     default:
       return state
